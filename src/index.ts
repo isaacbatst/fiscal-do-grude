789932531d94f79d54fbbf17a831baadce9ae85c
Isaac Batista <isaacbatst@gmail.com>
2020-12-12 05:44:37 -0600

    update create debtor command and message

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ import getAllDebtorsController from './useCases/GetAllDebtors';
 import manualIncrementController from './useCases/ManualReport';
 import speakingAboutController from './useCases/SpeakingAbout';
 
-bot.onText(/\/entrar/, msg => createDebtorController.handle(msg));
+bot.onText(/\/vale_introsa/, msg => createDebtorController.handle(msg));
 bot.onText(/\/listar_devedores/, msg => getAllDebtorsController.handle(msg))
 bot.onText(/vitor|victor/i, msg => speakingAboutController.handle(msg));
 bot.onText(/\/delatar_coleguinha/, msg => manualIncrementController.handle(msg));
+bot.onText(/\/iniciar_vaquinha/, msg => bot.sendMessage(msg.chat.id, 'Crio já...'));
+
 
 bot.on("polling_error", (msg) => console.log(msg));
